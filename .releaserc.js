c11bad180c6520dcbebd6fa36517ad96f5f2bb1e
Phil Ciampini <phil.ciampini@logdna.com>
2025-08-04 14:48:00 -0400

    fix(release): re-enable release notes￼
    we should be able to successfully generate release notes now.
    
    ref: LOG-22147

--- .releaserc.js
+++ .releaserc.js
@@ -1,13 +1,24 @@
 const logdnaConfig = require('@answerbook/release-config-logdna')
 
-const skipMe = '@semantic-release/release-notes-generator'
-
-module.exports = {
+const config = {
   ...logdnaConfig
-, plugins: logdnaConfig.plugins.filter(plugin => {
-    const pluginName = Array.isArray(plugin) ? plugin[0] : plugin
-    return pluginName !== skipMe
-  })
 , "branches": ["master"]
 , "changelogFile": "MEZMO_CHANGELOG.md"
 }
+/**
+ * NOTE: we've run into an issue where the generated release
+ * notes were too long to create a release when we've waited too
+ * long to pull in changes from up-stream.  This block remains
+ * so that we can easily disable the notes generator plugin in
+ * case we run into this scenario again.
+ *
+ *
+  const skipMe = '@semantic-release/release-notes-generator'
+  config.plugins = logdnaConfig.plugins.filter(plugin => {
+    const pluginName = Array.isArray(plugin) ? plugin[0] : plugin
+    return pluginName !== skipMe
+  })
+ *
+ */
+
+module.exports = config
